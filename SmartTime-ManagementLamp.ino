70db52dc55040f42272d8f9764c882d79f2df443
CoachSalimSagadatovich <salim.sagadatovich@gmail.com>
2016-01-31 16:22:20 +0300

    Create SmartTime-ManagementLamp.ino

---
+++ SmartTime-ManagementLamp.ino
@@ -0,0 +1,30 @@
+long cm;
+int d=12;
+void setup() {
+  pinMode(4,INPUT);
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+  pinMode(d,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(3,LOW);
+ delayMicroseconds(2);
+ digitalWrite(3,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(3,LOW);
+  a=pulseIn(4,HIGH);
+  cm=a/29/2;
+  Serial.print(cm);
+  Serial.print("CM");
+  Serial.println();
+  delay(500);
+     if (cm<20) { delay(4000); digitalWrite(d,HIGH);
+  delay(15000);
+   digitalWrite(d,LOW); delay(10000);}
+   else if (cm<20) digitalWrite(d,LOW);
+    if(cm>20) digitalWrite (d,LOW);
+       }
+  
+
+ 
